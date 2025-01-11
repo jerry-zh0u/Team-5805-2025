e4dcd0ccf99f3bba7c7047a4f226df61e7821484
Jerry Zhou <jerryzhou1011@gmail.com>
2025-01-10 22:00:04 -0800

    first commit

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,21 @@
+package frc.robot;
+
+import java.util.HashMap;
+
+import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
+
+public final class Constants{
+    public static class LimeLightConstants{
+       // Constants such as camera and target height stored. Change per robot and goal!
+        public static final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
+        public static final double TARGET_HEIGHT_METERS = Units.feetToMeters(4.8);
+
+        // Angle between horizontal and the camera.
+        public static final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(47);
+
+        // How far from the target we want to be
+        public static final double GOAL_RANGE_METERS = Units.feetToMeters(8);
+
+    }
+}
